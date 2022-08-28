b51aaa65ac12c4dff032f88728970605ac783e9c
lolitron <lolitron@lolitron>
2022-08-28 16:49:48 +0300

    cleanup

--- compile_and_run.sh
+++
@@ -1,6 +0,0 @@
-#g++ -o libEngine.so -std=c++17 -shared -fPIC *.cpp -I ./
-g++ -std=c++17 -c ./GLTest/*.cpp -I ./GLTest/ 
-g++ -std=c++17 -c ./WindowSystem/*.cpp -I ./WindowSystem/
-g++ "./GLTest/*.o ./WindowSystem/*.o" -o ./prog  -lGL -lglfw3 -lpthread -lm -lz -lGL -lX11 -lXext -lXfixes -ldl
-./prog
-rm *.o
